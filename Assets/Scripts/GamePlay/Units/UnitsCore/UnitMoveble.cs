e9021b1e788d4940666d71a0ad751c5dbd7abeac
Yuri Pokrepin <nipercop@mail.ru>
2025-08-05 19:15:30 +0600

    Created moveable and stationary units

---
+++ Assets/Scripts/GamePlay/Units/UnitsCore/UnitMoveble.cs
@@ -0,0 +1,14 @@
+using Game.GamePlayCore.Interfaces.Units;
+using UnityEngine;
+
+namespace Game.GamePlayCore.Units
+{
+    public class UnitMoveble : DamagableUnit , IMoveable
+    {
+        
+        public void UpdateMove(float deltaTime)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/GamePlay/Units/UnitsCore/UnitStationary.cs
@@ -0,0 +1,13 @@
+using Game.GamePlayCore.Interfaces.Units;
+using UnityEngine;
+
+namespace Game.GamePlayCore.Units
+{
+    public class UnitStationary : MonoBehaviour, IAttackable
+    {
+        public void UpdateAttack()
+        {
+            
+        }
+    }
+}
